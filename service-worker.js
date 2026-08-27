8aa70a62fd5ab2304ced49453871a0e16c0b680a
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-27 06:50:02 +0100

    Cache Symi teaching delivery build 0.19

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD='0.18.0';
+const BUILD='0.19.0';
 const CACHE=`symi-${BUILD}`;
 const ASSETS=[
   './','./index.html',
@@ -12,6 +12,7 @@ const ASSETS=[
   `./symi-brand-v017.js?v=${BUILD}`,
   `./symi-otj-share-v017.js?v=${BUILD}`,
   `./symi-course-hub-v018.js?v=${BUILD}`,
+  `./symi-teaching-v019.js?v=${BUILD}`,
   `./manifest.json?v=${BUILD}`,
   `./icon-192.png?v=${BUILD}`,
   `./icon-512.png?v=${BUILD}`,
